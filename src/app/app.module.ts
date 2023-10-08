63fb3aa07eea7ca59a4510b95d6f17cce9e2dded
salem <salemslimi0@gmail.com>
2023-10-08 22:21:53 +0200

    import module for logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideNavBarComponent } from './side-nav-bar/side-nav-bar.component';
+import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { SideNavBarComponent } from './side-nav-bar/side-nav-bar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
