1b7dd516b072e85a67cecf05b31b05e16482c412
salem <salemslimi0@gmail.com>
2023-10-14 10:32:43 +0200

    Add dashboard and statistics components to the menu bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,46 @@
 import {NgModule} from '@angular/core';
 import {Route, RouterModule} from '@angular/router';
 import {TransactionListComponent} from "./transaction/transaction-list/transaction-list.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {StatisticsComponent} from "./statistics/statistics.component";
+import {faChartPie, faCompass, faMoneyBillTransfer, IconDefinition} from "@fortawesome/free-solid-svg-icons";
+
 
 export type MenuRoute = Route & {
   label: string;
+  icon: IconDefinition;
 }
 
 export const routes: MenuRoute[] = [
+
+  {
+    label: 'Dashboard',
+    path: 'dashboard',
+    component: DashboardComponent,
+    icon: faCompass
+  },
   {
     label: 'Transactions',
     path: 'transactions',
-    component: TransactionListComponent
+    component: TransactionListComponent,
+    icon: faMoneyBillTransfer
+  },
+  {
+    label: 'Statistics',
+    path: 'statistics',
+    component: StatisticsComponent,
+    icon: faChartPie
   }
 ];
 
+const defaultRoute: Route = {
+  path: '',
+  redirectTo: '/dashboard',
+  pathMatch: 'full'
+}
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([defaultRoute, ...routes])],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideNavBarComponent } from './side-nav-bar/side-nav-bar.component';
 import {NgOptimizedImage} from "@angular/common";
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 
 @NgModule({
   declarations: [
     AppComponent,
-    SideNavBarComponent
+    SideNavBarComponent,
+    DashboardComponent,
+    StatisticsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FontAwesomeModule,
     NgOptimizedImage
   ],
   providers: [],
