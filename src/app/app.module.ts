20f94c2f3054d5dcb0c179eb213a06d36f846a95
salem <salemslimi0@gmail.com>
2024-01-08 22:25:09 +0100

    fix login: add token to request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,39 +8,40 @@ import {CommonModule, NgOptimizedImage} from "@angular/common";
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {StatisticsComponent} from './statistics/statistics.component';
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {TransactionModule} from "./transaction/transaction.module";
 import {PreferencesComponent} from './preferences/preferences.component';
 import {SignupComponent} from './signup/signup.component';
 import {LoginComponent} from "./login/login.component";
 import {ReactiveFormsModule} from "@angular/forms";
 import {JWT_OPTIONS, JwtHelperService} from "@auth0/angular-jwt";
-import {AuthService} from "./auth-service/auth.service";
+import {AuthorizationInterceptor} from "./auth-service/authorization.interceptor";
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        SideNavBarComponent,
-        DashboardComponent,
-        StatisticsComponent,
-        PreferencesComponent,
-        LoginComponent,
-        SignupComponent
-    ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        FontAwesomeModule,
-        HttpClientModule,
-        TransactionModule,
-        NgOptimizedImage,
-        CommonModule,
-        ReactiveFormsModule
-    ],
-    providers: [{provide: JWT_OPTIONS, useValue: JWT_OPTIONS},
-        JwtHelperService],
-    exports: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    SideNavBarComponent,
+    DashboardComponent,
+    StatisticsComponent,
+    PreferencesComponent,
+    LoginComponent,
+    SignupComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    TransactionModule,
+    NgOptimizedImage,
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  providers: [{provide: JWT_OPTIONS, useValue: JWT_OPTIONS},
+    JwtHelperService,
+    {provide: HTTP_INTERCEPTORS, useClass: AuthorizationInterceptor, multi: true}],
+  exports: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
---
+++ src/app/auth-service/authorization.interceptor.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {AuthService} from "./auth.service";
+
+@Injectable()
+export class AuthorizationInterceptor implements HttpInterceptor {
+
+  constructor(private authService: AuthService) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = localStorage.getItem("jwt-token");
+    if (token) {
+      const cloned = request.clone({
+        headers: request.headers.set('Authorization', `Bearer ${token}`)
+      });
+      return next.handle(cloned);
+    } else {
+      return next.handle(request);
+    }  }
+}
